f0eff8d2a122d9a76eedf015bef70721f3e38bd9
ekubechisimdi <chisimdiekube61@gmail.com>
2022-07-21 13:59:39 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("Hello")
+let name="simdi"
+console.log(name)
+let simdi="simdi"+"Ekube"
+console.log(simdi)
+let word=2+2
+console.log(2+2)
+let x=2
+let y=4
+let add=x+y
+let subtract=y-x
+let divide=y/x
+let multiply=y*x
+console.log(add)
+console.log(subtract)
+console.log(divide)
+console.log(multiply)
+let equals= x==y
+console.log(equals)
+let notequals=x!=y
+console.log(notequals)
+    
+
+
