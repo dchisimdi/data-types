fbc97e50a8ccd136f2e7d4dfc6b9b5927abfc2bf
ekubechisimdi <chisimdiekube61@gmail.com>
2022-07-27 12:11:56 +0100

    i did some practicals

--- index.js
+++ index.js
@@ -18,7 +18,36 @@ console.log(multiply)
 let equals= x==y
 console.log(equals)
 let notequals=x!=y
-console.log(notequals)
-    
-
-
+console.log(notequals)   
+let person = null;
+let emptyArray =[];
+let numbers = [1,2,3];
+let randomArray = [1,"best",true,null]
+let MyName = "simdi Ekube"
+console.log(MyName);
+console.log(numbers);
+console.log(numbers[2]);
+console.log(randomArray[3])
+console.log(numbers.length)
+console.log(emptyArray.length)
+console.log(randomArray.length)
+let newArray =[5,6,1,2,5,6,7,8,10,100,0,13];
+console.log(newArray)
+let lastposition = newArray.length-1;
+console.log(newArray[lastposition]);
+let newobject ={}
+let personalobject ={
+    name: "simdi ekube",
+    age: 13,
+    job: "a junior software developer",
+    isRich: true,
+}
+console.log(13)
+console.log(personalobject)
+console.log(personalobject.name)
+console.log(personalobject.job)
+console.log(personalobject.age)
+console.log(personalobject["name"])
+console.log(personalobject["age"])
+console.log(personalobject["job"])
+console.log(personalobject["isRich"])
